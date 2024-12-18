49cdcd59059b0d331735c9b927f8f39bcf236e0c
Ali Raheel <aliraheel626@gmail.com>
2024-12-18 16:33:03 +0500

    third commit

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,19 +2,19 @@ import { useState } from "react";
 import api from "../api";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import { useNavigate } from "react-router-dom";
-
-function Form(route, method) {
+import "../styles/Form.css";
+function Form({ route, method }) {
+  console.log(method);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const name = method === "login" ? "Login" : "Register";
   const handleSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
 
     try {
-      const res = await api.post("route", { username, password });
+      const res = await api.post(route, { username, password });
       if (method === "login") {
         localStorage.setItem(ACCESS_TOKEN, res.data.access);
         localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
@@ -31,7 +31,7 @@ function Form(route, method) {
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
-      <h1>{name}</h1>
+      <h1>{method === "login" ? "Login" : "Register"}</h1>
       <input
         className="form-input"
         type="text"
@@ -47,8 +47,10 @@ function Form(route, method) {
         placeholder="password"
       />
       <button className="form-button" type="submit">
-        {name}
+        {method === "login" ? "Login" : "Register"}
       </button>
     </form>
   );
 }
+
+export default Form;
